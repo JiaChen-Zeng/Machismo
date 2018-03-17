206ef6c02de20c864b6fbda8349de88c10fc2504
彩月葵 <aoi@ayatsukiaois-Mac.local>
2018-03-17 13:52:49 +0800

    Initial Commit

---
+++ Machismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Machismo
+//
+//  Created by 彩月葵 on 2018/03/17.
+//  Copyright © 2018年 彩月葵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Machismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Machismo
+//
+//  Created by 彩月葵 on 2018/03/17.
+//  Copyright © 2018年 彩月葵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
