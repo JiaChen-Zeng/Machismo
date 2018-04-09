b8825bdbd8ed5d6378aa6aa1e6c17df01f849dd5
彩月葵 <aoi@ayatsukiaois-mac.local>
2018-04-09 15:32:04 +0800

    FIX: Init numbers

--- Model/Set/SetCardDeck.m
+++ Model/Set/SetCardDeck.m
@@ -15,14 +15,16 @@
     self = [super init];
     if (!self) return nil;
     
-    for (NSString *symbol  in [SetCard validSymbols]) {
-        for (NSString *shading in [SetCard validShadings]) {
-            for (NSString *color in [SetCard validColor]) {
-                SetCard *card = [[SetCard alloc] init];
-                card.symbol = symbol;
-                card.shading = shading;
-                card.color = color;
-                [self addCard:card];
+    for (unsigned number = 1; number < [SetCard maxNumber]; ++number) {
+        for (NSString *symbol  in [SetCard validSymbols]) {
+            for (NSString *shading in [SetCard validShadings]) {
+                for (NSString *color in [SetCard validColor]) {
+                    SetCard *card = [[SetCard alloc] init];
+                    card.symbol = symbol;
+                    card.shading = shading;
+                    card.color = color;
+                    [self addCard:card];
+                }
             }
         }
     }
